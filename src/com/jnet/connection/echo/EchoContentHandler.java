a8fa50a950e1eff4d5d9ffd6e329a9d871f6bcba
yangxunwu <yangxunwu@outlook.com>
2020-12-29 21:50:32 +0800

    update EchoContentHandler

--- src/com/jnet/connection/echo/EchoContentHandler.java
+++ src/com/jnet/connection/echo/EchoContentHandler.java
@@ -19,18 +19,4 @@ public class EchoContentHandler extends ContentHandler {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         return bufferedReader.readLine();
     }
-
-    @Override
-    public Object getContent(URLConnection connection, Class[] classes) throws IOException {
-        InputStream inputStream = connection.getInputStream();
-        for(int i = 0; i < classes.length; i++) {
-            if(classes[i] == InputStream.class) {
-                return inputStream;
-            }else if(classes[i] == String.class) {
-                return getContent(connection);
-            }
-        }
-
-        return null;
-    }
 }
