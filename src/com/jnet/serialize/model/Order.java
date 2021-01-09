8fd53c99a69627cf13853f1364da8f8f20d92bf8
yangxunwu <yangxunwu@outlook.com>
2021-01-09 17:19:35 +0800

    add serialVersionUID

--- src/com/jnet/serialize/model/Order.java
+++ src/com/jnet/serialize/model/Order.java
@@ -1,5 +1,7 @@
 package com.jnet.serialize.model;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
+
 import java.io.Serializable;
 
 /**
@@ -7,11 +9,23 @@ import java.io.Serializable;
  * @version 1.0.0
  */
 public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
     private Customer customer;
     private String orderName;
+    private Double price;
+
+    public Order() {
+    }
 
     public Order(String orderName) {
+        this(orderName, 0.0);
+    }
+
+    public Order(String orderName, Double price) {
         this.orderName = orderName;
+        this.price = price;
     }
 
     public Customer getCustomer() {
@@ -22,10 +36,27 @@ public class Order implements Serializable {
         this.customer = customer;
     }
 
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     @Override
     public String toString() {
         return "Order{" +
-                "orderName='" + orderName + '\'' +
+                ", orderName='" + orderName + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
